75d2f26e3edba981e09fad0ed800fbec49a9d822
matteoorig <matteoorig@gmail.com>
2021-12-21 16:44:31 +0100

    update

--- back-end/app.js
+++ back-end/app.js
@@ -1,1 +1,11 @@
-//prima riga
+var net = require('net');
+var server = net.createServer();
+server.listen(8899);
+
+server.on('connection', (con) =>{
+
+    con.on("data", (data) =>{
+        console.log(data.toString());
+    });
+ 
+});
