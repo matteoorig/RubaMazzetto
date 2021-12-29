2fb9bf1c721b8a2fdbad912a225138b69a72bbbc
matteoorig <matteoorig@gmail.com>
2021-12-29 16:44:35 +0100

    update

--- back-end/app.js
+++ back-end/app.js
@@ -1,36 +1,12 @@
-const net = require("net");
+var net = require('net');
 
-var stato = 0; //stato=0 => può accettare richieste     stato=1 => non deve accettare richieste perchè già occupato
-var CLIENT = null;
-var HOST = null;
+var server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.pipe(socket);
 
-//in ascolto continuo
-const PeerHandlerListener = net.createServer();
-PeerHandlerListener.on("connection", (so) => {
-    CLIENT = so;
-    CLIENT.on("data", (data) =>{
-        console.log("[SERVER] " + data);
-        //non funge
-        if(data.equals(Buffer.from("Hi"))){
-            connect(8889, '127.0.0.1')
-            console.log("ci siamo")
-        }
-    });
-});
-
-PeerHandlerListener.listen(8889, ()=>{
-    console.log("[PeerHandlerListener] pronto");
-});
-
-//da richiamare quando si vuole fare la richiesta di inizio partita
-function connect(port, ip){
-   // PeerHandlerListener.close();
-    HOST = net.createConnection(port, ip);
-    HOST.on("connect", () =>{
-        HOST.write("Hello");
-
+    socket.on("data", (data)=>{
+        console.log("data"+data);
     })
-}
+});
 
-
-
+server.listen(1337, '127.0.0.1');
