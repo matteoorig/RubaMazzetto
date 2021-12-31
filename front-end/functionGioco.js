5c354e17b932f97f86e1a54559410413f1ce0f4b
EliaSaetti <eli.saee03@gmail.com>
2021-12-31 13:22:42 +0100

    Aggiornamento grafica

---
+++ front-end/functionGioco.js
@@ -0,0 +1,11 @@
+/*function impostaImg(){
+
+    console.log("ciao");
+    let src = document.getElementById("select_image").value;
+    src.replaceAll('\',"/");
+    console.log(src);
+    let newText = '<img src="'+src+'">';
+    console.log(newText);
+    document.getElementById("target").innerHTML=newText;
+}*/
+
